5f9d9d9a726507c7c3a90a706616ad1a8a0735f5
pratheep-8 <154980242+pratheep-8@users.noreply.github.com>
2026-04-01 04:46:56 +0000

    Updataion on 01-04-2026

---
+++ .sfdx/typings/lwc/apex/CustomSearchController.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/CustomSearchController.searchRecords" {
+  export default function searchRecords(param: {objName: any, fieldName: any, searchKey: any}): Promise<any>;
+}
